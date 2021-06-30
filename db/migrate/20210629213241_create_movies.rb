d699f849b41c19745a5eab534e847dcbd02b8812
Paul Veliscovici <paul.veliscovici@gmail.com>
2021-06-30 14:14:34 -0400

    first table created in the db

--- db/migrate/20210629213241_create_movies.rb
+++ db/migrate/20210629213241_create_movies.rb
@@ -1,6 +1,12 @@
 class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
+      t.string :title
+      t.string :description
+      t.string :language
+      t.string :language
+
+
 
       t.timestamps
     end
