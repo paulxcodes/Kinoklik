451095d38ef4243fc660d2e22815d64b3b622a0b
dumitrescu <dumitrescucostin20@gmail.com>
2021-08-18 13:34:48 -0400

    mux gem for video upload installed

---
+++ config/initializers/mux.rb
@@ -0,0 +1,4 @@
+MuxRuby.configure do |config|
+  config.username = ENV["aa28ed73-80da-4d98-8c43-ed9ad073855b"]
+  config.password = ENV["McaVq9n63yaUrBNJB5shxzE+lv4iScazGZOJ0v2IiV+VoWvbn+mU315Lf0IvbjZVYLOj+DWImoC4"]
+end
