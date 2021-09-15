8a9af1f6a6c07efda3d592a4974ae5f715f112d1
Rohit Badyal <rohitbadyal@Rohits-MacBook-Pro.local>
2021-09-15 12:44:20 -0400

    added movie_image to model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,6 +6,8 @@ class Movie < ApplicationRecord
   has_many :actors
   has_one :user_id
 
+  has_one_attached :movie_image
+
   validates :title, presence: true
   validates :description, presence: true
   validates :language, presence: true
